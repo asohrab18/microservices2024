9b81e3b7936605cf4cbb4fba2d031b41c95b4a80
Sohrab Alam <sohrab.alam86@gmail.com>
2024-01-05 15:13:08 +0530

    Fault Tolerance by default Retry using Resilience4j

---
+++ exchange-currency-service/src/main/java/com/microservices/controller/CircuitBreakerController.java
@@ -0,0 +1,26 @@
+package com.microservices.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.retry.annotation.Retry;
+
+@RestController
+@RequestMapping("circuit-breaker")
+public class CircuitBreakerController {
+
+	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
+	@GetMapping("sample-api")
+	@Retry(name = "default")
+	public String getSample() {
+		logger.info("=====>sample API call received.<========");
+		ResponseEntity<String> re = new RestTemplate().getForEntity("http://localhost:8080/dummy-api", String.class);
+		return re.getBody();
+	}
+}
